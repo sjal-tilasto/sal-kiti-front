2b2b90621420e321db2023ed072fbd44bbc7be0f
Jukka Karvonen <jukka.karvonen@iki.fi>
2021-08-13 14:05:46 +0300

    Add interceptor to force reload if spa-version header changes

--- src/api/BaseApi.js
+++ src/api/BaseApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import i18n from "../i18n";
+import store from "../store";
 
 /**
  * Creates an Axios instance for API calls
@@ -16,3 +17,28 @@ HTTP.interceptors.request.use(function(config) {
   config.headers["Accept-Language"] = i18n.locale;
   return config;
 });
+
+HTTP.interceptors.response.use(
+  function(response) {
+    const versionHeader = "spa-version";
+    if (
+      versionHeader in response.headers &&
+      parseInt(response.headers[versionHeader])
+    ) {
+      let apiVersion = parseInt(response.headers[versionHeader]);
+      let currentVersion = store.state.spaVersion;
+      if (currentVersion) {
+        if (currentVersion < apiVersion) {
+          store.commit("setSPAVersion", apiVersion);
+          window.location.reload(true);
+        }
+      } else {
+        store.commit("setSPAVersion", apiVersion);
+      }
+    }
+    return response;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
